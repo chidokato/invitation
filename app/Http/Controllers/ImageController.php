b25fab90a48914b3343f5b757bea800f08f9f654
chidokato <tuan.pn92@gmail.com>
2025-01-13 15:35:18 +0700

    sdf

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -22,13 +22,14 @@ class ImageController extends Controller
 
         // Lưu file ảnh upload với tên duy nhất
         $uploadedFile = $request->file('img');
-        $uniqueAvatarName = uniqid('avatar_') . rand(0, 99) . '.' . $uploadedFile->getClientOriginalExtension();
-        $avatarPath = public_path('images/' . $uniqueAvatarName);
-        $uploadedFile->move(public_path('images'), $uniqueAvatarName);
+        $uniqueId = uniqid(); // Tạo ID duy nhất
+        $avatarPath = public_path("images/avatar_{$uniqueId}.png");
+        $uploadedFile->move(public_path('images'), "avatar_{$uniqueId}.png");
+
 
         // Tạo ảnh nền và vẽ text + avatar
-        $baseImagePath = public_path('images/thumoi.png');
-        $outputPath = public_path('images/generated_thumoi.png');
+        $baseImagePath = public_path('images/thumoi-1.jpg');
+        $outputPath = public_path("images/generated_thumoi_{$uniqueId}.png");
         $fontPath = public_path('fonts/arial.ttf');
 
         $image = Image::make($baseImagePath);
@@ -36,25 +37,32 @@ class ImageController extends Controller
         $avatar = Image::make($avatarPath);
 
         // 1. Resize chiều ngang về 300px, giữ tỉ lệ
-        $avatar->fit(300, 300, function ($constraint) {
+        $avatar->fit(344, 344, function ($constraint) {
             $constraint->upsize();
         });
 
         // 2. Crop chính giữa thành hình vuông 300x300
-        $x = (int)(($avatar->width() / 2) - (300 / 2));
-        $y = (int)(($avatar->height() / 2) - (300 / 2));
-        $avatar->crop(300, 300, $x, $y);
+        $x = (int)(($avatar->width() / 2) - (344 / 2));
+        $y = (int)(($avatar->height() / 2) - (344 / 2));
+        $avatar->crop(344, 344, $x, $y);
 
 
         // 3. Tạo mặt nạ tròn để bo tròn avatar
-        $mask = Image::canvas(300, 300);
-        $mask->circle(300, 150, 150, function ($draw) {
+        $mask = Image::canvas(344, 344);
+        $mask->circle(344, 172, 172, function ($draw) {
             $draw->background('#fff');
         });
         $avatar->mask($mask, false);
 
         // 4. Chèn avatar đã bo tròn vào ảnh nền
-        $image->insert($avatar, 'top-left', 0, 0); // Điều chỉnh tọa độ avatar
+        $backgroundWidth = $image->width();
+        $avatarWidth = $avatar->width();
+        $x = ($backgroundWidth - $avatarWidth) / 2; // Căn giữa theo chiều ngang
+        $y = 264; // Cách top 200px
+
+        // Chèn avatar vào ảnh nền
+        $image->insert($avatar, 'top-left', (int)$x, $y);
+
 
         // 5. Thêm text vào ảnh
         $image->text($fullName, $image->width() / 2, 160, function ($font) use ($fontPath) {
@@ -72,7 +80,7 @@ class ImageController extends Controller
         @unlink($avatarPath);
 
         // Trả về view để hiển thị ảnh
-        return view('show_image', ['imagePath' => asset('images/generated_thumoi.png')]);
+        return view('show_image', ['imagePath' => asset("images/generated_thumoi_{$uniqueId}.png")]);
     }
 
 
